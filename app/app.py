b2e84d88a1c305cbee47d6041c39ef5c5e549109
Dhayananth <dhayananthd624@gmail.com>
2026-06-27 11:16:54 +0530

    test real looking secrets

--- app/app.py
+++ app/app.py
@@ -2,4 +2,6 @@ def greet(name):
     return f"hello {name} from secure app"
 print(greet("krish"))
 
-AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "abc123secretkeyfordhaya9876"
+DB_PASSWORD = "mysupersecretpassword123"
