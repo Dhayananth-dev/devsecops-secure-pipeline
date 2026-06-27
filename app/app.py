26c224326f354c856cb27bca43bb6e55ee5e00fc
Dhayananth <dhayananthd624@gmail.com>
2026-06-27 11:20:51 +0530

    fix: remove leaked secrets

--- app/app.py
+++ app/app.py
@@ -1,7 +1,3 @@
 def greet(name):
     return f"hello {name} from secure app"
 print(greet("krish"))
-
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET_ACCESS_KEY = "abc123secretkeyfordhaya9876"
-DB_PASSWORD = "mysupersecretpassword123"
